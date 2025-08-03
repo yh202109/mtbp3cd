f71c4899ffceaba710ab3c78a4edc323477e1419
yh202109 <yh202109@gmail.com>
2025-08-03 11:34:00 -0400

    init

--- mtbp3cd/gui/__init__.py
+++ mtbp3cd/gui/__init__.py
@@ -1,1 +1,11 @@
 from .appfileinventory import *
+
+import sys
+from PyQt6.QtWidgets import QApplication
+
+def runapp(type="FileInventory"):
+    if isinstance(type, str) and type == "FileInventory":
+        app = QApplication(sys.argv)
+        windows = FileInventoryApp()
+        windows.show() 
+        sys.exit(app.exec())
